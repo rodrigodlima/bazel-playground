521dada97506961da96ffeebd14152d9c5a12606
Rodrigo de Lima Silva <rodrigodlima@gmail.com>
2026-01-05 09:59:46 -0300

    Refactor media calculation output to CSV format and update .gitignore to include CSV files

--- apps/media_calc/__main__.py
+++ apps/media_calc/__main__.py
@@ -1,3 +1,6 @@
+import csv
+
+
 def calc_media(notes):
     temp = 0
     for i in range(len(notes)):
@@ -8,7 +11,11 @@ def calc_media(notes):
 
 
 notas = [7, 10, 8, 6]
+name = "Rodrigo"
+file_path = "final_result.csv"
 result = calc_media(notas)
 
-with open("final_result.txt", "w") as file:
-    file.write(f"The media is: {result}")
+with open(file_path, "w", newline='') as file:
+    writer = csv.writer(file, delimiter=',', quotechar='"',
+                        quoting=csv.QUOTE_MINIMAL)
+    writer.writerow([name, int(result)])
