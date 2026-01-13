3b1aa732f73fff0ac9a91ae81b7c32b69b18085d
Rodrigo de Lima Silva <rodrigodlima@gmail.com>
2026-01-12 22:31:47 -0300

    Add example code to save data on MongoDB

--- apps/student_management/__main__.py
+++ apps/student_management/__main__.py
@@ -1,2 +1,26 @@
-import pymongo
+from pymongo import MongoClient
+
+import os
 from dotenv import load_dotenv
+
+load_dotenv()
+
+CONNECTION_STRING = os.getenv('connection_string')
+print(CONNECTION_STRING)
+DB_NAME = os.getenv('db_name')
+
+client = MongoClient(CONNECTION_STRING)
+dbname = client[DB_NAME]
+
+# CRUD
+
+collection_name = dbname["students"]
+
+student1 = {
+    "Name": "Rodrigo",
+    "Surname": "Lima",
+    "Note 1": "8",
+    "Note 2": "6"
+}
+
+collection_name.insert_one(student1)
